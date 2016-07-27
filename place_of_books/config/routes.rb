f7bc248c2f0bfecf46f7fc85bcdb7f7ea1c9080b
Austen Lockwood <austenlockwood@gmail.com>
2016-07-26 22:31:37 -0400

    controller and model generated

--- place_of_books/config/routes.rb
+++ place_of_books/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  get '/books' => "books#index"
+  get '/books/:id' => "books#show"
+  post '/books' => "books#create"
+  patch '/books/:id' => "books#edit"
+  delete 'books/:id' => "books#delete"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
